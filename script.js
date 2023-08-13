1fe50143f1b0133e6dad8a7cbb3a1b8e3a4a7fad
KevinRwema <kevinrwema@gmail.com>
2023-08-13 22:43:17 +0200

    Filter countries based on continent, now working

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
-// const h3 = document.getElementById('h3')
-// const p1 = document.getElementById('p1')
-// const p2 = document.getElementById('p2')
-// const p3 = document.getElementById('p3')
-// const img = document.getElementById('img')
+
 let flagsContainer = document.getElementById('flags-container')
 const selectContinent = document.getElementById('select')
-let selectedContinent ="";
+let continent = "";
 
-selectContinent.addEventListener('change',function(){
-   selectedContinent = selectContinent.value
-   
-   
+function resetDOM() {
+  console.log("Yay!")
+  flagsContainer.textContent = "";
+}
+
+function updateDOM() {
+  continent = selectContinent.value;
+  let apiUrlByContinent = "";
+  if (continent === "all") {
+    apiUrlByContinent = 'https://restcountries.com/v3.1/all'
+  } else {
+    apiUrlByContinent = `https://restcountries.com/v3.1/region/${continent}`
+  }
+
+  fetch(apiUrlByContinent)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+
+
+      for (const country of data) {
+        let countryDiv = document.createElement('div');
+        countryDiv.className = 'country';
+        let card = document.createElement('div');
+        card.className = 'card';
+        let img = document.createElement('img')
+        img.setAttribute('src', country.flags.png)
+        let h3 = document.createElement('h3');
+        h3.textContent = country.name.common;
+        let p1 = document.createElement('p');
+        p1.textContent = `Population: ${country.population}`;
+        let p2 = document.createElement('p');
+        p2.textContent = `Continent: ${country.continents}`;
+        let p3 = document.createElement('p');
+        p3.textContent = `Capital: ${country.capital}`;
+        card.appendChild(img)
+        card.appendChild(h3)
+        card.appendChild(p1)
+        card.appendChild(p2)
+        card.appendChild(p3)
+        countryDiv.appendChild(card)
+        flagsContainer.appendChild(countryDiv)
+
+      }
+
+
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+
+  console.log(continent)
+}
+
+
+selectContinent.addEventListener('change', function () {
+  resetDOM();
+  updateDOM();
 })
 
-
-
-
-
-
 // Replace with the actual API endpoint URL
-const apiUrl = `https://restcountries.com/v3.1/region/Americas`
+const apiUrl = 'https://restcountries.com/v3.1/all'
 
 /* 
  By All : 'https://restcountries.com/v3.1/all'
@@ -47,21 +96,21 @@ fetch(apiUrl)
     // p2.innerText = `Region: ${randomCountry.continents[0]}`;
     // p3.innerText = `Capital: ${randomCountry.capital[0]}`;
 
-    for (const country of data){
+    for (const country of data) {
       let countryDiv = document.createElement('div');
       countryDiv.className = 'country';
       let card = document.createElement('div');
       card.className = 'card';
       let img = document.createElement('img')
-      img.setAttribute('src',country.flags.png)
+      img.setAttribute('src', country.flags.png)
       let h3 = document.createElement('h3');
       h3.textContent = country.name.common;
       let p1 = document.createElement('p');
-      p1.textContent =`Population: ${country.population}`;
+      p1.textContent = `Population: ${country.population}`;
       let p2 = document.createElement('p');
-      p2.textContent= `Continent: ${country.continents}`;
+      p2.textContent = `Continent: ${country.continents}`;
       let p3 = document.createElement('p');
-      p3.textContent = `Capital: ${country.capital[0]}`;
+      p3.textContent = `Capital: ${country.capital}`;
       card.appendChild(img)
       card.appendChild(h3)
       card.appendChild(p1)
